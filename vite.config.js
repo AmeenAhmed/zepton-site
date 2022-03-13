e879527e2cbca3426f9768bdbd783c9358070a50
Ameen Ahmed <ameen.ahmed.b@gmail.com>
2022-03-13 19:38:57 +0530

    Generate multi page site

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
+import { resolve } from 'path';
+
 export default {
   server: {
     port: 3200
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        tutorial: resolve(__dirname, 'tutorial/index.html'),
+        docs: resolve(__dirname, 'docs/index.html'),
+        examples: resolve(__dirname, 'examples/index.html'),
+        playground: resolve(__dirname, 'playground/index.html'),
+      }
+    }
   }
 }
